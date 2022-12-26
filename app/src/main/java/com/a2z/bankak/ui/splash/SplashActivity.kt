99245f74e0049e1a81ce05b0c90e3a7495dba3eb
ahmedzenhom <ahmed.a.zenhom@gmail.com>
2022-12-26 20:06:17 +0200

    Finalizing SplashActivity UI

--- app/src/main/java/com/a2z/bankak/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/a2z/bankak/ui/splash/SplashActivity.kt
@@ -1,10 +1,20 @@
 package com.a2z.bankak.ui.splash
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.view.WindowManager
 import androidx.activity.viewModels
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import com.a2z.bankak.R
 import com.a2z.bankak.core.base.BaseActivity
 import com.a2z.bankak.databinding.NewLayoutSplashActivityBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity<NewLayoutSplashActivityBinding, SplashViewModel>() {
@@ -13,10 +23,51 @@ class SplashActivity : BaseActivity<NewLayoutSplashActivityBinding, SplashViewMo
     override val binding by viewBinding(NewLayoutSplashActivityBinding::inflate)
 
     override fun onActivityCreated() {
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
+        initUI()
     }
 
+    @SuppressLint("SetTextI18n")
+    private fun initUI() {
+        with(binding) {
+            tvAppVersion.text = "V:4.42"
+            Glide
+                .with(this@SplashActivity)
+                .asGif()
+                .load(R.drawable.splashgif)
+                .listener(GifRequestListener())
+                .into(ivSplash)
+        }
+    }
+
+    private fun checkNavigation(){
+        showSuccessMsg("GIF ENDED")
+    }
+
+    private inner class GifRequestListener : RequestListener<GifDrawable> {
+        override fun onLoadFailed(
+            e: GlideException?, m: Any?, t: Target<GifDrawable>?, r: Boolean
+        ): Boolean {
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: GifDrawable?,
+            model: Any?,
+            target: Target<GifDrawable>?,
+            dataSource: DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            resource?.let {
+                it.setLoopCount(1)
+                it.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+                    override fun onAnimationEnd(drawable: Drawable?) {
+                        super.onAnimationEnd(drawable)
+                        checkNavigation()
+                    }
+                })
+            }
+            return false
+        }
+
+    }
 }
