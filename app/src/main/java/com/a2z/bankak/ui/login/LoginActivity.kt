be3f5aaef1350d361ffd156f925a7451520bd92c
ahmedzenhom <ahmed.a.zenhom@gmail.com>
2022-12-26 21:29:08 +0200

    Finalizing LoginActivity UI

--- app/src/main/java/com/a2z/bankak/ui/login/LoginActivity.kt
+++ app/src/main/java/com/a2z/bankak/ui/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.a2z.bankak.ui.login
 import android.content.Context
 import android.content.Intent
 import androidx.activity.viewModels
+import com.a2z.bankak.R
 import com.a2z.bankak.core.base.BaseActivity
 import com.a2z.bankak.databinding.ActivityNewLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +19,22 @@ class LoginActivity : BaseActivity<ActivityNewLoginBinding, LoginViewModel>() {
     override val binding by viewBinding(ActivityNewLoginBinding::inflate)
 
     override fun onActivityCreated() {
+        initUI()
+    }
+
+    private fun initUI() = with(binding) {
+        loginBtn.setOnClickListener {
+            val id = edtmbCif.text.toString().trim()
+            val password = edmbPwd.text.toString().trim()
+            if (id.isEmpty() || password.isEmpty()) {
+                showWarningMsg(getString(R.string.fieldsEmp_Err))
+                return@setOnClickListener
+            }
+            startLogin(id, password)
+        }
+    }
+
+    private fun startLogin(id: String, password: String) {
 
     }
 }
